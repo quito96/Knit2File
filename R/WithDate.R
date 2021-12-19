cadc3c22a61293e0e32380f061a1e3b9e6d8f32d
Quito96 <37045252+quito96@users.noreply.github.com>
2021-12-19 12:05:51 +0100

    New Setup

--- R/WithDate.R
+++ R/WithDate.R
@@ -9,7 +9,7 @@ WithDateHTML <- function(input, ...) {
         rmarkdown::render(
                 input,
                 output_file = paste0(
-                        xfun::sans_ext(input), '_', Sys.Date(), '_.',
+                        xfun::sans_ext(input), '_', Sys.Date(), '_',
                         xfun::file_ext("html_document")
                 ),
                 envir = globalenv()
